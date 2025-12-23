1002df35beb8aca22c58064f6c778c1357a9486a
ZixunHuangUPenn <huang98@seas.upenn.edu>
2025-12-22 22:25:14 -1000

    fix(npm): ignore npm source map warning

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,20 +1,14 @@
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.module.rules.forEach((rule) => {
-        if (!rule.oneOf) return;
-
-        rule.oneOf.forEach((loader) => {
-          if (
-            loader.loader &&
-            loader.loader.includes('source-map-loader')
-          ) {
-            loader.exclude = /node_modules/;
-          }
-        });
-      });
-
-      return webpackConfig;
+    configure: (config) => {
+      config.module.rules = config.module.rules.filter(
+        (rule) =>
+          !(
+            rule.loader &&
+            rule.loader.includes('source-map-loader')
+          )
+      );
+      return config;
     },
   },
 };
